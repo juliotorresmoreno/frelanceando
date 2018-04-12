bd905ef8cd665f9c577a2838e8b824fbfa308da6
Your Name <jtorres990@gmail.com>
2018-04-11 22:47:11 -0500

    fix

--- src/pages/SignUp/FormInput.jsx
+++ src/pages/SignUp/FormInput.jsx
@@ -5,47 +5,21 @@ import {
 } from 'antd';
 
 export default ({ name, label, type = 'text', children, value, onChange = () => { }, ...rest }) => (
-    type !== 'select' ?
-        <div {...rest}>
-            <Form.Item label={label}>
+    <div {...rest}>
+        <Form.Item label={label}>
+            {type !== 'select' ?
                 <Input
                     type={type} name={name}
-                    id={name} label={label} 
+                    id={name} label={label}
                     value={value} onChange={onChange}
-                    style={{ borderRadius: 20, height: 30 }}/>
-            </Form.Item>
-        </div> :
-        <div {...rest}>
-            <Form.Item label={label}>
+                    style={{ borderRadius: 20, height: 30 }} /> :
                 <Select
                     type={type} name={name}
                     id={name} label={label}
                     value={value} onChange={onChange}
                     style={{ borderRadius: 20, height: 30 }}>
                     {children}
-                </Select>
-            </Form.Item>
-        </div>
+                </Select>}
+        </Form.Item>
+    </div>
 )
-
-/*
-import React from 'react';
-import {
-    FormGroup, Label, Input
-} from 'reactstrap';
-
-export default ({ name, label, type = 'text', children, onChange = () => { }, ...rest }) => (
-    <FormGroup {...rest}>
-        <Label
-            for={name}
-            style={{ marginBottom: 0 }}>
-            {label}
-        </Label>
-        <Input
-            type={type} name={name} id={name}
-            style={{ borderRadius: 20, height: 30 }}>
-            {children}
-        </Input>
-    </FormGroup>
-);
-*/
